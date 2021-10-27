3c87e7ad652ceddd8e3dbc685c91c68288607a06
vgenij-asiliev <vasiliev.evgenij.aleksandrovich@gmail.com>
2021-10-27 10:33:46 +0300

    Hello, React Native!!!

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.content}>Hello, React Native!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'yellow',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  content: {
+    fontSize: 100,
+    fontWeight: 'bold',
+    color: 'red'    
+  }
 });
